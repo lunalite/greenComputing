b6aca911b53274dcf3b40cc809cdecb2d6bcd50c
hdk216 <hd.koh290293@gmail.com>
2017-03-03 00:58:56 +0000

    initial wq

---
+++ test1/test.java
@@ -0,0 +1,9 @@
+class test {
+public static void main (String args[]) {
+
+for (int i = 100000000; i >= 0 ; i --) {
+	System.out.println(i);
+}
+}
+
+}
---
+++ test2/test2.java
@@ -0,0 +1,12 @@
+class test2 {
+public static void main (String args[]) {
+
+for (int i = 100000000; i >= 0 ; i --) {
+	System.out.println(i);
+	for (int j = 0; j <= 100; j ++) {
+	System.out.println(j);
+	}
+}
+}
+
+}
